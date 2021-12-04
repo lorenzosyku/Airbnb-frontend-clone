b364160f005729ce445202a81818595ea6e78a8c
lorenzo syku <lorenzosyku@lorenzos-MacBook-Pro.local>
2021-12-04 11:54:47 -0500

    implemented search page

---
+++ pages/search.js
@@ -0,0 +1,69 @@
+import { useRouter } from "next/dist/client/router";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import InfoCard from "../components/InfoCard";
+
+function Search({searchResults}) {
+
+  
+  const router = useRouter();
+  console.log(searchResults)
+  const {location, startDate, endDate, noOfGuests} = router.query;
+
+  const formattedStartDate = startDate.split('T')[0];
+  const formattedEndDate = endDate.split('T')[0];
+
+  //const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+  //const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+  const range = `from ${formattedStartDate} to ${formattedEndDate}`;
+
+  return (
+    <div>
+      <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">300+ stays-- {range} --for {noOfGuests} guests{" "}</p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button px-4 py-2 rounded-full border cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">Cancellation Flexibility</p>
+            <p className="button px-4 py-2 rounded-full border cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">Type of place</p>
+            <p className="button px-4 py-2 rounded-full border cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">Price</p>
+            <p className="button px-4 py-2 rounded-full border cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">Rooms and Beds</p>
+            <p className="button px-4 py-2 rounded-full border cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">More filters</p>
+          </div>
+          <div className="flex flex-col">
+            {searchResults.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
+        </section>
+      </main>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Search;
+
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://links.papareact.com/isz").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
